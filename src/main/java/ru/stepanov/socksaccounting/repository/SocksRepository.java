f4ef4da757ee93bf48dbf5923871a14526d0538a
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-09-24 13:18:55 +0300

    add Jpa repository to make queries to DB

---
+++ src/main/java/ru/stepanov/socksaccounting/repository/SocksRepository.java
@@ -0,0 +1,9 @@
+package ru.stepanov.socksaccounting.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.stepanov.socksaccounting.dao.SockDao;
+
+@Repository
+public interface SocksRepository extends JpaRepository<SockDao, Long> {
+}
