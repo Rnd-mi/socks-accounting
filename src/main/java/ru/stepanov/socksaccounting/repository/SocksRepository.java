d8046dbb47ee4b6444f5176c30525b4a916c2886
Ivan Stepanov <11.stepanov.01@gmail.com>
2023-09-24 20:01:52 +0300

    feature: add 3 methods to repository for advanced search based on given parameters

--- src/main/java/ru/stepanov/socksaccounting/repository/SocksRepository.java
+++ src/main/java/ru/stepanov/socksaccounting/repository/SocksRepository.java
@@ -1,9 +1,35 @@
 package ru.stepanov.socksaccounting.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.stepanov.socksaccounting.model.Sock;
 
+import java.util.Optional;
+
 @Repository
-public interface SocksRepository extends JpaRepository<Sock, Integer> {
+public interface SocksRepository extends JpaRepository<Sock, String> {
+    Optional<Sock> findByColorIgnoreCaseAndCottonPart(String color, short cottonPart);
+
+    @Query(
+            value = "select sum(quantity) from socks where color = :color and cotton_part = :cottonPart",
+            nativeQuery = true
+          )
+    Optional<Integer> findByCottonPartEquals(@Param("color") String color,
+                                             @Param("cottonPart") short cottonPart);
+
+    @Query(
+            value = "select sum(quantity) from socks where color = :color and cotton_part > :cottonPart",
+            nativeQuery = true
+          )
+    Optional<Integer> findByCottonPartMoreThan(@Param("color") String color,
+                                               @Param("cottonPart") short cottonPart);
+
+    @Query(
+            value = "select sum(quantity) from socks where color = :color and cotton_part < :cottonPart",
+            nativeQuery = true
+          )
+    Optional<Integer> findByCottonPartLessThan(@Param("color") String color,
+                                               @Param("cottonPart") short cottonPart);
 }
